eb2472e752708e04ee4041d04638ef56c79e3c83
devbipu <devbipu@gmail.com>
2023-02-07 21:43:23 +0600

    Migration Added

---
+++ database/migrations/2023_02_07_152412_create_files_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('orginal_name');
+            $table->string('storage_path');
+            $table->string('view_path');
+            $table->integer('file_size');
+            $table->string('file_type');
+            $table->integer('user_id');
+            $table->morphs('fileable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+};
